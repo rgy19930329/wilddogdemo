dfc20e584950e5da2176f562c165c28ecaefc8c2
冉光宇 <rgy@ranguangyudeMacBook-Pro.local>
2017-01-17 23:41:13 +0800

    rgy

--- wilddogutils.js
+++ wilddogutils.js
@@ -13,9 +13,26 @@
     });
   };
 
-
+  // 移除数据
   wu.remove = function(ref, id, opts) {
+    ref.child(id).remove()
+    .then(function() {
+      pts && opts.success && opts.success();
+    })
+    .catch(function(error) {
+      opts && opts.error && opts.error(err);
+    });
+  };
 
+  // 添加数据
+  wu.add = function(ref, item, opts) {
+    ref.push(item)
+    .then(function() {
+      opts && opts.success && opts.success();
+    })
+    .catch(function(err) {
+      opts && opts.error && opts.error(err);
+    });
   };
 
 })(window);
